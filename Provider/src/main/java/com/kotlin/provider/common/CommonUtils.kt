22cdb81341d76404f4d8734d7c7e25e7c4989436
Stark.Tony <stark.tony.industory@gmail.com>
2019-05-29 20:42:50 +0800

    1.格式化代码

--- Provider/src/main/java/com/kotlin/provider/common/CommonUtils.kt
+++ Provider/src/main/java/com/kotlin/provider/common/CommonUtils.kt
@@ -8,7 +8,7 @@ import com.kotlin.provider.router.RouterPath
 /*
     顶级函数，判断是否登录
  */
-fun isLogined():Boolean{
+fun isLogined(): Boolean {
     return AppPrefsUtils.getString(BaseConstant.KEY_SP_TOKEN).isNotEmpty()
 }
 
@@ -16,10 +16,10 @@ fun isLogined():Boolean{
     如果已经登录，进行传入的方法处理
     如果没有登录，进入登录界面
  */
-fun afterLogin(method:()->Unit){
-    if (isLogined()){
+fun afterLogin(method: () -> Unit) {
+    if (isLogined()) {
         method()
-    }else{
+    } else {
         ARouter.getInstance().build(RouterPath.UserCenter.PATH_LOGIN).navigation()
     }
 }
