9c0484f2d29ac08c3580171c4c348bb3832e5789
Stark.Tony <stark.tony.industory@gmail.com>
2019-05-30 18:02:49 +0800

    增加 BaseLazyFragment

---
+++ BaseLibrary/src/main/java/com/kotlin/base/ui/fragment/BaseLazyFragment.kt
@@ -0,0 +1,46 @@
+package com.kotlin.base.ui.fragment
+
+import android.app.Fragment
+import android.os.Bundle
+import android.support.annotation.UiThread
+
+/*
+    懒加载fragment基类
+ */
+abstract class BaseLazyFragment : Fragment() {
+
+    /**
+     * 懒加载过
+     */
+    private var isLazyLoaded: Boolean = false
+
+    private var isPrepared: Boolean = false
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        isPrepared = true
+        //只有Fragment onCreateView好了，
+        //另外这里调用一次lazyLoad(）
+        lazyLoad()
+    }
+
+    override fun setUserVisibleHint(isVisibleToUser: Boolean) {
+        super.setUserVisibleHint(isVisibleToUser)
+        lazyLoad()
+    }
+
+    /**
+     * 调用懒加载
+     */
+
+    private fun lazyLoad() {
+        if (userVisibleHint && isPrepared && !isLazyLoaded) {
+            onLazyLoad()
+            isLazyLoaded = true
+        }
+    }
+
+    @UiThread
+    abstract fun onLazyLoad()
+
+}
